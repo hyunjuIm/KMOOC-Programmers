6d583b2a03750d5baaa97ba1bc121b508f677dab
hyunju <dear.jjwim@gmail.com>
2022-09-27 16:17:05 +0900

    Json 파싱

--- app/src/main/java/com/programmers/kmooc/network/HttpClient.kt
+++ app/src/main/java/com/programmers/kmooc/network/HttpClient.kt
@@ -9,18 +9,23 @@ class HttpClient(private val baseUrl: String) {
 
     fun getJson(path: String, params: Map<String, Any>, completed: (Result<String>) -> Unit) {
         GlobalScope.launch(Dispatchers.IO) {
-            var queryParams = if (params.isNotEmpty()) params
-                .map { entry -> "${entry.key}=${entry.value}" }
-                .reduce { s1, s2 -> "${s1}&${s2}" }
-            else ""
+            try {
+                var queryParams = if (params.isNotEmpty()) params
+                    .map { entry -> "${entry.key}=${entry.value}" }
+                    .reduce { s1, s2 -> "${s1}&${s2}" }
+                else ""
 
-            if (queryParams.isNotEmpty()) queryParams = "?${queryParams}"
+                if (queryParams.isNotEmpty()) queryParams = "?${queryParams}"
 
-            val url = if (path.startsWith("http")) URL(path + queryParams)
-            else URL(baseUrl + path + queryParams)
+                val url = if (path.startsWith("http")) URL(path + queryParams)
+                else URL(baseUrl + path + queryParams)
 
-            val json = url.readText(Charsets.UTF_8)
-            completed(Result.success(json))
+                val json = url.readText(Charsets.UTF_8)
+                completed(Result.success(json))
+            } catch (e: Exception) {
+                e.printStackTrace()
+                completed(Result.failure(e))
+            }
         }
     }
 
--- app/src/main/java/com/programmers/kmooc/repositories/KmoocRepository.kt
+++ app/src/main/java/com/programmers/kmooc/repositories/KmoocRepository.kt
@@ -25,6 +25,9 @@ class KmoocRepository {
             result.onSuccess {
                 completed(parseLectureList(JSONObject(it)))
             }
+            result.onFailure {
+                completed(LectureList.EMPTY)
+            }
         }
     }
 
@@ -33,6 +36,9 @@ class KmoocRepository {
         httpClient.getJson(nextPageUrl, emptyMap()) { result ->
             result.onSuccess {
                 completed(parseLectureList(JSONObject(it)))
+            }
+            result.onFailure {
+                completed(LectureList.EMPTY)
             }
         }
     }
@@ -45,16 +51,49 @@ class KmoocRepository {
             result.onSuccess {
                 completed(parseLecture(JSONObject(it)))
             }
+            result.onFailure {
+                completed(Lecture.EMPTY)
+            }
         }
     }
 
     private fun parseLectureList(jsonObject: JSONObject): LectureList {
         //TODO: JSONObject -> LectureList 를 구현하세요
-        return LectureList.EMPTY
+        return jsonObject.getJSONObject("pagination").run {
+            LectureList(
+                count = getInt("count"),
+                numPages = getInt("num_pages"),
+                previous = optString("previous"),
+                next = getString("next"),
+                lectures = getJSONArray("results").run {
+                    mutableListOf<Lecture>().apply {
+                        for (i in 0 until length()) {
+                            add(parseLecture(getJSONObject(i)))
+                        }
+                    }
+                }
+            )
+        }
     }
 
     private fun parseLecture(jsonObject: JSONObject): Lecture {
         //TODO: JSONObject -> Lecture 를 구현하세요
-        return Lecture.EMPTY
+        return Lecture(
+            id = jsonObject.getString("id"),
+            number = jsonObject.getString("number"),
+            name = jsonObject.getString("name"),
+            classfyName = jsonObject.getString("classfy_name"),
+            middleClassfyName = jsonObject.getString("middle_classfy_name"),
+            courseImage = jsonObject.getJSONObject("media").getJSONObject("image")
+                .getString("small"),
+            courseImageLarge = jsonObject.getJSONObject("media").getJSONObject("image")
+                .getString("large"),
+            shortDescription = jsonObject.getString("short_description"),
+            orgName = jsonObject.getString("org_name"),
+            start = DateUtil.parseDate(jsonObject.getString("start")),
+            end = DateUtil.parseDate(jsonObject.getString("end")),
+            teachers = jsonObject.optString("teachers") ?: null,
+            overview = jsonObject.optString("html") ?: null,
+        )
     }
 }
